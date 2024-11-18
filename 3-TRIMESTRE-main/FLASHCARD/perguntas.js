08a91b6b2b1730146fe0126d96d3a38ae3f34b1a
gabriel <138135290+Gagabiel@users.noreply.github.com>
2024-11-18 08:48:37 -0300

    Add files via upload

---
+++ 3-TRIMESTRE-main/FLASHCARD/perguntas.js
@@ -0,0 +1,68 @@
+criaCartao(
+    ' Geografia',
+    'Qual o maior estado brasileiro em extensão territorial?',
+    'Amazonas'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o marco que deu início à Idade Média?',
+    'A queda do Império Romano do Ocidente, em 476 d.C.'
+)
+
+criaCartao(
+    ' História / Ciências Espaciais',
+    'Quem foi o primeiro ser humano a viajar para o espaço?',
+    'Yuri Gagarin, em 12 de abril de 1961'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quem é considerado o "pai" da sociologia?',
+    ' Auguste Comte'
+)
+
+
+criaCartao(
+    'Matemática',
+    'Quantos lados tem um hexágono?',
+    '6 lados'
+)
+
+
+criaCartao(
+    'Química',
+    'Quem foi o cientista que organizou a primeira versão da tabela periódica?',
+    'Dmitri Mendeleev'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual o plural de child?',
+    'Children'
+)
+
+
+criaCartao(
+    'Artes',
+    'Qual pintor espanhol é conhecido por obras como "Guernica"?',
+    'Pablo Picasso'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Português',
+    'Quem escreveu "O primo Basílio"?',
+    'José de Alencar'
+)
